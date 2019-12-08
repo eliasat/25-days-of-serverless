313ba6bb978bfe4577e3713481f5ee9be95762f8
Elias Al-Tai <elias.altai@dekiru.se>
2019-12-08 21:11:22 +0100

    initial commit, challenge 1

---
+++ week-1/challenge-1/Challenge-1/Dreidel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Challenge_1
+{
+    public static class Dreidel
+    {
+        [FunctionName("Dreidel")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var rnd = new Random();
+            var value = rnd.NextDouble();
+            string resp;
+            if (value < 0.33)
+            {
+                resp = "נ";
+            }
+            else if (value < 0.66)
+            {
+                resp = "ג";
+            }
+            else
+            {
+                resp = "ה";
+            }
+            return new OkObjectResult(resp);
+        }
+    }
+}
